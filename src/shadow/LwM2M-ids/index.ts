1f84f6ce54aa89f25d611234b8268a6a2b55e22e
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-11-30 13:39:58 +0100

    feat: Map shadow's name values with LwM2M IDs Object and Properties

---
+++ src/shadow/LwM2M-ids/index.ts
@@ -0,0 +1,26 @@
+import ConnectivityMonitoring from "./ConnectivityMonitoring.json";
+import ECIDSignalMeasurementInformation from "./ECID-SignalMeasurementInformation.json";
+import Device from "./Device.json";
+import FirmwareUpdate from "./FirmwareUpdate.json";
+import Humidity from "./Humidity.json";
+import Location from "./Location.json";
+import LwM2MServer from "./LwM2MServer.json";
+import Pressure from "./Pressure.json";
+import PushButton from "./PushButton.json";
+import Temperature from "./Temperature.json";
+
+/**
+ * Map shadow's name values with LwM2M IDs Object and Properties
+ */
+export const LwM2MIds = {
+  ConnectivityMonitoring,
+  ECIDSignalMeasurementInformation,
+  Device,
+  FirmwareUpdate,
+  Humidity,
+  Location,
+  LwM2MServer,
+  Pressure,
+  PushButton,
+  Temperature,
+};
