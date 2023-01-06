aaac021ae693346751a7de9de4e4fa1e870bf558
Mauro López <mauro.lopez@nordicsemi.no>
2023-01-06 16:14:54 +0100

    feat: return URN of resource (#2)
    Return the URN of the resource following the next format:
    
            `'<"oma"|"ext"|"x">:<ObjectID>:<ObjectVersion>@<LWM2MVersion>'`
    
    If 'ObjectVersion' and/or 'LWM2MVersion' equal to '1.0', value is omitted.

---
+++ src/updateResourceId/index.spec.ts
@@ -0,0 +1,22 @@
+import { getURN } from "./index";
+
+describe("Update resource id", () => {
+  it.each([
+    ["3303", "3303:1.1"],
+    ["3", "3:1.2@1.1"],
+    ["4", "4:1.3@1.1"],
+    ["10256", "10256"],
+    ["5", "5:1.1@1.1"],
+    ["3304", "3304:1.1"],
+    ["6", "6"],
+    ["1", "1:1.2@1.2"],
+    ["3323", "3323:1.1"],
+    ["3347", "3347:1.1"],
+    ["3303", "3303:1.1"],
+  ])(
+    "Should check if the full URN of resource %s is %s ",
+    async (value, expected) => {
+      expect(await getURN(value)).toBe(expected);
+    }
+  );
+});
