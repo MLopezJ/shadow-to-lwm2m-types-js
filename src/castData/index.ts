21b6010668ece40853721350480263c2ad311382
Mauro Lopez <maurolopez@Mauros-MacBook-Pro.local>
2023-01-06 22:06:04 +0100

    WIP: cast data from coiote shadow using json schema

---
+++ src/castData/index.ts
@@ -0,0 +1,22 @@
+import sc from "schema-casting";
+
+/**
+ * Draft schema of 3303
+ */
+const schema = {
+  $schema: "http://json-schema.org/draft-04/schema#",
+  type: "object",
+  properties: {
+    "5518": { required: true, type: "integer" },
+  },
+  required: true,
+  additionalProperties: false,
+};
+
+const input = {
+  "5518": "1665149633",
+};
+
+const output = sc(schema, input);
+// { '5518': 1665149633 }
+console.log(output);
