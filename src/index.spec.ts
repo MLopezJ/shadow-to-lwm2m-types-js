5a5706cc4d6010224c5611147f2b315880d6de4c
Mauro Lopez <mauro.lopez@nordicsemi.com>
2023-01-12 17:27:01 +0100

    fix: split main function Divide main function in 2: 1- main 2- steps
    The main function is just an interface for the user. it takes as parameters the shadow and the ids.
    The steps functions is for the shadow transformation, It contains all the logic.
    
    Having this 2 concerns separately allows better testing and increase readability of code.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,9 @@
-import { main } from "./index";
-import coioteLwM2MJsonShcema from "./input/coioteLwM2MJsonShcema.schema.json";
+import { main, steps } from "./index";
 import { LwM2MIds } from "./input/LwM2M-ids";
+import jsonSchema from "../node_modules/@nordicsemiconductor/lwm2m-types/LwM2MDocument.schema.json"; // TODO: export json schema from lib
 
 describe("main", () => {
-  it("should test complete flow of process", async () => {
+  it("should test shadow transformation", async () => {
     const input = {
       state: {
         reported: {
@@ -69,13 +69,147 @@ describe("main", () => {
       ],
     };
 
-    
-    const result = await main(input, coioteLwM2MJsonShcema,LwM2MIds )
+    const result = await main(input, LwM2MIds);
     expect(Object.keys(input.state.reported).length).toEqual(
       Object.keys(result).length
     );
     expect(result).toStrictEqual(expected);
     expect(result).not.toHaveProperty("ECID-Signal Measurement Information");
     expect(result).toHaveProperty("10256");
+  });
+
+  it("should test process with different inputs", async () => {
+    const shadow = {
+      state: {
+        reported: {
+          Temperature: {
+            "0": {
+              "Sensor Units": "Celsius degrees",
+              "Sensor Value": "24.57",
+            },
+          },
+        },
+      },
+    };
+
+    const anotherShadow = {
+      state: {
+        reported: {
+          "new name here": {
+            "0": {
+              hello: "Celsius degrees",
+              hi: "24.57",
+            },
+          },
+        },
+      },
+    };
+
+    const anotherIdsObject = {
+      "new name here": {
+        id: "3303",
+        name: "new name here",
+        properties: {
+          hi: "5700",
+          hello: "5701",
+        },
+      },
+    };
+
+    const expected = {
+      "3303:1.1": [
+        {
+          "5700": 24.57,
+          "5701": "Celsius degrees",
+        },
+      ],
+    };
+
+    expect(await main(shadow, LwM2MIds)).toStrictEqual(expected);
+    expect(await main(anotherShadow, anotherIdsObject as any)).toStrictEqual(
+      expected
+    );
+  });
+});
+
+describe("steps", () => {
+  it("should test methods are called with expected params", async () => {
+    const shadow = {
+      state: {
+        reported: {
+          Temperature: {
+            "0": {
+              "Sensor Units": "Celsius degrees",
+              "Sensor Value": "24.57",
+              "Fractional Timestamp": {
+                noValue: true,
+              },
+            },
+          },
+        },
+      },
+    };
+
+    const resultStep1 = {
+      Temperature: [
+        {
+          "Sensor Units": "Celsius degrees",
+          "Sensor Value": "24.57",
+          "Fractional Timestamp": {
+            noValue: true,
+          },
+        },
+      ],
+    };
+
+    const resultStep2 = {
+      Temperature: [
+        { "Sensor Units": "Celsius degrees", "Sensor Value": "24.57" },
+      ],
+    };
+
+    const resultStep3 = {
+      "3303": [{ "5700": "24.57", "5701": "Celsius degrees" }],
+    };
+
+    const resultStep4 = {
+      "3303:1.1": [{ "5700": "24.57", "5701": "Celsius degrees" }],
+    };
+
+    // step 1
+    const fromMapToPlainObject = jest
+      .fn()
+      .mockImplementation(() => resultStep1);
+
+    // step 2
+    const removeNotProvidedValues = jest
+      .fn()
+      .mockImplementation(() => resultStep2);
+
+    // step 3
+    const nameToId = jest.fn().mockImplementation(() => resultStep3);
+
+    // step 4
+    const fromIdToUrn = jest.fn().mockImplementation(() => resultStep4);
+
+    // step 5
+    const sc = jest.fn();
+
+    await steps(
+      shadow.state.reported,
+      LwM2MIds,
+      jsonSchema,
+      fromMapToPlainObject,
+      removeNotProvidedValues,
+      nameToId,
+      fromIdToUrn,
+      sc
+    );
+
+    expect(fromMapToPlainObject).toHaveBeenCalledWith(shadow.state.reported); // step 1
+    expect(removeNotProvidedValues).toHaveBeenCalledWith(resultStep1); // step 2
+    expect(nameToId).toHaveBeenCalledWith(resultStep2, LwM2MIds); // step 3
+    expect(fromIdToUrn).toHaveBeenCalledWith(resultStep3); // step 4
+    expect(sc).toHaveBeenCalledWith(jsonSchema, resultStep4); // step 5
   });
 });
