5d86cc650f7f5e11adee1eb9a9005a293d2f9444
Mauro Lopez <mauro.lopez@nordicsemi.com>
2023-01-12 17:33:38 +0100

    fix: update example

--- src/example.ts
+++ src/example.ts
@@ -1,6 +1,5 @@
 import coioteShadow from "./input/shadow.json";
 import { LwM2MIds } from "./input/LwM2M-ids";
-import jsonSchema from "../node_modules/@nordicsemiconductor/lwm2m-types/LwM2MDocument.schema.json"; // TODO: export json schema from lib
 import { main } from "./index";
 
-main(coioteShadow, jsonSchema, LwM2MIds);
+main(coioteShadow, LwM2MIds);
