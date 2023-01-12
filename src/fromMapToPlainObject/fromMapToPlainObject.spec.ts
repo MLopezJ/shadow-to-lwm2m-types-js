ddac0e47a58ab8b23086fab6972fe5d894d02607
Mauro Lopez <mauro.lopez@nordicsemi.com>
2023-01-12 20:26:40 +0100

    test: transform object taking json schema as ref

--- src/fromMapToPlainObject/fromMapToPlainObject.spec.ts
+++ src/fromMapToPlainObject/fromMapToPlainObject.spec.ts
@@ -1,4 +1,3 @@
-
 import { fromMapToPlainObject } from "./fromMapToPlainObject";
 
 describe("fromMapToPlainObject()", () => {
@@ -77,6 +76,68 @@ describe("fromMapToPlainObject()", () => {
     };
 
     expect(fromMapToPlainObject(value)).toStrictEqual(expected);
-    expect(Object.keys(value).length).toEqual(Object.keys(fromMapToPlainObject(value)).length)
+    expect(Object.keys(value).length).toEqual(
+      Object.keys(fromMapToPlainObject(value)).length
+    );
+  });
+  it("should transform map taking in consideration the json schema definition", () => {
+    // Location is an object by schema definition
+    const value = {
+      Location: {
+        "0": {
+          Altitude: "0.0",
+          Latitude: "0.0",
+          Longitude: "0.0",
+          Radius: "0.0",
+          Speed: "0.0",
+          Timestamp: "1970-01-01T00:00:00Z",
+          Velocity: "",
+        },
+      },
+    };
+    const expected = {
+      "6": {
+        "0": "0.0",
+        "1": "0.0",
+        "2": "0.0",
+        "3": "0.0",
+        "5": "1970-01-01T00:00:00Z",
+        "6": "0.0",
+      },
+    };
+    expect(fromMapToPlainObject(value)).toStrictEqual(expected);
+  });
+  it("should transform map into array", () => {
+    // Pressure is an array by the schema definition
+    const value = {
+      Pressure: {
+        "0": {
+          "Application Type": "",
+          "Max Measured Value": "98.236",
+          "Max Range Value": "110.0",
+          "Min Measured Value": "98.236",
+          "Min Range Value": "30.0",
+          "Sensor Units": "kPa",
+          "Sensor Value": "98.226",
+          Timestamp: "2022-10-07T13:33:22Z",
+        },
+      },
+    };
+    const expected = {
+      Pressure: [
+        {
+          "Application Type": "",
+          "Max Measured Value": "98.236",
+          "Max Range Value": "110.0",
+          "Min Measured Value": "98.236",
+          "Min Range Value": "30.0",
+          "Sensor Units": "kPa",
+          "Sensor Value": "98.226",
+          Timestamp: "2022-10-07T13:33:22Z",
+        },
+      ],
+    };
+
+    expect(fromMapToPlainObject(value)).toStrictEqual(expected);
   });
 });
